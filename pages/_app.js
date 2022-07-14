890930a47b4af24872ae89c31fdc928d7655bc25
Alejandro Mazas de Lizana <alejandro.mazasdelizana@gmail.com>
2022-07-14 20:54:43 +0200

    navbar start

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Navibar from '../components/navibar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <>
+      <Navibar />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
---
+++ pages/proyectos.js
@@ -0,0 +1,12 @@
+const proyectos = () => {
+
+    return (
+        <>
+            <div>
+                <h1>Proyectos</h1>
+            </div>
+        </>
+    )
+}
+
+export default proyectos
